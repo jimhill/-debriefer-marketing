47d3163a12370404f675363c235e58ca2ccc9b69
Jim Hill <ukjimbobbery@gmail.com>
2026-02-04 11:35:17 +0000

    Added dot grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,34 @@
 import Image from 'next/image';
+import DotGrid from './components/dot-grid';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-      <Image
-        className="dark:invert"
-        src="/debriefer.svg"
-        alt="Debriefer logo"
-        width={330}
-        height={62}
-        priority
-      />
-      <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-        <h1 className="max-w-xs text-3xl font-medium leading-10 tracking-tight text-zinc-500 dark:text-zinc-50">
-          Coming Soon
+    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-12 px-16 sm:py-32 bg-white dark:bg-black sm:items-start">
+      <div className="flex flex-col items-center gap-4 text-center sm:items-start sm:text-left">
+        <Image
+          className="dark:invert w-[150px] h-[28px] sm:w-[200px] sm:h-[40px]"
+          src="/debriefer.svg"
+          alt="Debriefer logo"
+          width={330}
+          height={62}
+          priority
+        />
+        <h1 className="max-w-xs text-base sm:text-xl font-medium leading-tight tracking-tight text-zinc-500 dark:text-zinc-50 text-pretty">
+          Building blocks for gathering deep qualitative research fast.
         </h1>
+      </div>
+      <div className="w-full min-h-[200px] h-[200px] relative">
+        <DotGrid
+          dotSize={5}
+          gap={15}
+          baseColor="#271E37"
+          activeColor="#5227FF"
+          proximity={120}
+          shockRadius={250}
+          shockStrength={5}
+          resistance={750}
+          returnDuration={1.5}
+        />
       </div>
     </main>
   );
