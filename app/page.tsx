4294f5fa3e6b83692ff3db47ed7379c52b4af51d
Jim Hill <ukjimbobbery@gmail.com>
2026-02-04 12:15:51 +0000

    Slight change in messaging

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import DotGrid from './components/dot-grid';
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-12 px-16 sm:py-32 bg-white dark:bg-black sm:items-start">
-      <div className="flex flex-col items-center gap-4 text-center sm:items-start sm:text-left">
+      <div className="flex flex-col gap-2 items-start text-left">
         <Image
           className="dark:invert w-[150px] h-[28px] sm:w-[200px] sm:h-[40px]"
           src="/debriefer.svg"
@@ -13,9 +13,16 @@ export default function Home() {
           height={62}
           priority
         />
-        <h1 className="max-w-xs text-base sm:text-xl font-medium leading-tight tracking-tight text-zinc-500 dark:text-zinc-50 text-pretty">
-          Building blocks for gathering deep qualitative research fast.
+        <h1 className="mt-2 max-w-xl text-xl sm:text-2xl font-medium leading-tight tracking-tight text-zinc-500 dark:text-zinc-50 text-pretty">
+          Gather deep qualitative research through interviews fast.
         </h1>
+        <h2 className="max-w-xl text-base font-semibold leading-tight tracking-tight text-zinc-900 dark:text-zinc-50 text-pretty">
+          The tools you need to design, conduct, and analyze your own AI-run
+          interviews rapidly and at scale.
+        </h2>
+        <p className="max-w-xl text-sm text-zinc-500 dark:text-zinc-50 text-pretty">
+          Coming soon: APIs, SDKs, MCPs, Editors, and more.
+        </p>
       </div>
       <div className="w-full min-h-[200px] h-[200px] relative">
         <DotGrid
